64366e9fe33c58af07100bfb970d5041913ab883
Stephan Linkel <251381+norganos@users.noreply.github.com>
2022-12-09 08:49:41 +0100

    day 9 part 2

--- jvm/src/main/kotlin/de/linkel/aoc/Day09.kt
+++ jvm/src/main/kotlin/de/linkel/aoc/Day09.kt
@@ -1,18 +1,19 @@
 package de.linkel.aoc
 
 import de.linkel.aoc.base.AbstractLinesAdventDay
+import io.micronaut.context.annotation.Value
 import jakarta.inject.Singleton
 import kotlin.math.abs
 import kotlin.math.max
 
 @Singleton
-class Day09: AbstractLinesAdventDay<Day09.Result>() {
+class Day09(
+    @Suppress("MnInjectionPoints") @Value("1") val length: Int = 9
+): AbstractLinesAdventDay<Day09.Result>() {
     override val day = 9
 
     override fun process(lines: Sequence<String>): Result {
-        val hStart = Point(0, 0)
-        val tStart = Point(0, 0)
-        val rope = Rope(hStart, tStart)
+        val rope = Rope(length)
 
         lines.forEach { line ->
             val tt = line.split(' ')
@@ -22,27 +23,28 @@ class Day09: AbstractLinesAdventDay<Day09.Result>() {
             }
         }
 
-        return Result(rope.tailPoints.size)
+        return Result(length, rope.tailPoints.size)
     }
 
     class Rope(
-        headStart :Point,
-        tailStart: Point
+        length: Int
     ) {
-        private var tail: Point = tailStart
-        private var head: Point = headStart
-        val tailPoints = mutableSetOf<Point>(tailStart)
+        private var knots: Array<Point> = List(length + 1) { _ -> Point(0, 0)}.toTypedArray()
+        val tailPoints = mutableSetOf(Point(0, 0))
 
         fun moveHead(direction: Direction) {
-            head += direction.vector
-            val t2h = head - tail
-            if (t2h.distance >= 2) {
-                tail += Vector(
-                    deltaX = if (t2h.deltaX == 0) 0 else t2h.deltaX / abs(t2h.deltaX),
-                    deltaY = if (t2h.deltaY == 0) 0 else t2h.deltaY / abs(t2h.deltaY)
-                )
-                tailPoints.add(tail)
+            knots[0] = knots[0] + direction.vector
+
+            for (i in 1 until knots.size) {
+                val v = knots[i-1] - knots[i]
+                if (v.distance >= 2) {
+                    knots[i] = knots[i] + Vector(
+                        deltaX = if (v.deltaX == 0) 0 else v.deltaX / abs(v.deltaX),
+                        deltaY = if (v.deltaY == 0) 0 else v.deltaY / abs(v.deltaY)
+                    )
+                }
             }
+            tailPoints.add(knots.last())
         }
     }
 
@@ -79,10 +81,11 @@ class Day09: AbstractLinesAdventDay<Day09.Result>() {
     }
 
     data class Result(
+        val length: Int,
         val tailTrailLength: Int
     ) {
         override fun toString(): String {
-            return "tail touched $tailTrailLength points"
+            return "tail of length $length touched $tailTrailLength points"
         }
     }
 }
--- jvm/src/test/kotlin/Day09Test.kt
+++ jvm/src/test/kotlin/Day09Test.kt
@@ -18,23 +18,50 @@ R 2
     fun `example part 1`() {
         assertEquals(
             13,
-            Day09().test(aocExample).tailTrailLength
+            Day09(1).test(aocExample).tailTrailLength
         )
     }
 
     @Test
     fun `example part 2`() {
+        assertEquals(
+            1,
+            Day09().test(aocExample).tailTrailLength
+        )
+    }
+
+    val aocExample2 = """
+R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20
+    """.trimIndent()
+
+    @Test
+    fun `bigger example part 2`() {
+        assertEquals(
+            36,
+            Day09().test(aocExample2).tailTrailLength
+        )
     }
 
     @Test
     fun `solution part 1`() {
         assertEquals(
             6337,
-            Day09().solve(emptyList()).tailTrailLength
+            Day09(1).solve(emptyList()).tailTrailLength
         )
     }
 
     @Test
     fun `solution part 2`() {
+        assertEquals(
+            2455,
+            Day09().solve(emptyList()).tailTrailLength
+        )
     }
 }
